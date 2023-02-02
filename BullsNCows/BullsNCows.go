52d9f67e681fb79866f8d5ce6934e8df98ad3f4b
BustrovVladiSlaveIS-21 <119808497+NotAProger@users.noreply.github.com>
2023-02-02 18:28:41 +0300

    Add files via upload

--- BullsNCows/BullsNCows.go
+++ BullsNCows/BullsNCows.go
@@ -10,28 +10,25 @@ import (
 	"time"
 )
 
-func randomNum() int {
+func randomNum(a, n int) int {
 	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(9)
-	return n
+	r := rand.Intn(n) + a
+	return r
 }
 
 func randomNums() []int {
 	for {
 		count := 0
-		nums := []int{randomNum(), randomNum(), randomNum(), randomNum()}
-
-		if nums[0] != 0 {
-			count++
-			for g := range nums {
-				for i := range nums {
-					if nums[g] != nums[i] {
-						count++
-					}
+		nums := []int{randomNum(1, 8), randomNum(0, 9), randomNum(0, 9), randomNum(0, 9)}
+		for g := range nums {
+			for i := range nums {
+				if nums[g] != nums[i] {
+					count++
 				}
 			}
 		}
-		if count == 13 {
+
+		if count == 12 {
 			return nums
 		}
 	}
@@ -43,20 +40,20 @@ func readerCnsl() string {
 		fmt.Print("Write in your guess number -> ")
 		text, _ := reader.ReadString('\n')
 		textR := text
-		if len(textR) < 6 {
-			text = ""
-			if textR != "" {
-				return textR
-			}
+		text = ""
+		if len(textR) < 7 && textR != "" {
+			return textR
+		} else if textR == "" {
+			fmt.Println("Put in some number")
 		} else {
-			fmt.Println("You put in too big input")
+			fmt.Println("You put in too big number")
 		}
 	}
 }
 func decompGuessNum(s string) []int {
 	splited := strings.Split(s, "")
 	var numI []int
-	numI = make([]int, 6)
+	numI = make([]int, 7)
 
 	for i, r := range splited {
 		numI[i], _ = strconv.Atoi(r)
@@ -64,25 +61,25 @@ func decompGuessNum(s string) []int {
 	return numI[0:4]
 }
 
-func compare(nums, guess []int) (cow, bull int) {
-	for i, r := range guess {
-		if guess[i] == nums[i] {
-			bull++
-		}
-		for f := range nums {
-			if r == nums[f] {
-				cow++
+func compare(nums, guess []int, t int) {
+	bull := 0
+	cow := 0
+
+	for i, g := range guess {
+		for j, n := range nums {
+			if g == n {
+				if i == j {
+					bull++
+				} else {
+					cow++
+				}
 			}
 		}
 	}
-	cow = cow - bull
-	return bull, cow
-}
 
-func conditions(bull, cow, turns int) {
 	if bull == 4 {
 		fmt.Println("")
-		fmt.Printf("Cowgratulations, you won, it takes %d turns.", turns)
+		fmt.Printf("Cowgratulations, you won, it takes %d turns.", t)
 		fmt.Println("")
 		os.Exit(1)
 	} else {
@@ -91,16 +88,16 @@ func conditions(bull, cow, turns int) {
 		fmt.Println("")
 	}
 }
+
 func main() {
-	turns := 1
+	turns := 0
 	fmt.Println("Number game Bools and Cows")
 	nums := randomNums()
 	fmt.Println("Nums are ready!")
 	for {
+		turns++
 		guessStr := readerCnsl()
 		guess := decompGuessNum(guessStr)
-		bull, cow := compare(nums, guess)
-		conditions(bull, cow, turns)
-		turns++
+		compare(nums, guess, turns)
 	}
 }
